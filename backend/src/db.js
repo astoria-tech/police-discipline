870cd3ef5e327709a5d646fb0d6a13de609e239c
Ash Ryan Arnwine <accounts@ashryan.io>
2020-07-08 15:59:39 -0400

    Error handling for fs read.

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,10 +1,23 @@
 const fs = require("fs");
 
 const getData = () => {
-  const jsonData = fs.readFileSync('./data/db.json', 'utf8');
-  const parsedData = JSON.parse(jsonData);
+  const filepath = './data/db.json';
 
-  return parsedData;
+  try {
+    const jsonData = fs.readFileSync(`${filepath}`, 'utf8');
+    const parsedData = JSON.parse(jsonData);
+    
+    return parsedData;
+  } catch (error) {
+    if (err.code === 'ENOENT') {
+      console.log(`${filepath} file not found!`);
+    } else if (err.code == 'EACCES') {
+      console.log(`Not allowed to read ${filepath}`);
+      
+    } 
+      
+    throw error;
+  }
 }
 
 module.exports = getData();
